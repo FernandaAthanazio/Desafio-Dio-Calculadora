c8ab8bc07f18fd73ad8ebb45d8766bf3051744cb
Fernanda <fernandaathanazio@gmail.com>
2022-12-17 18:55:56 -0300

    Desafio-Dio-Calculadora

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #E8EAB5;
+    background-color: #E7A4DF;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
